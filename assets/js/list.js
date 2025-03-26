a4de029f8fed69fea486efb691cdbfd0d711fb5f
debasiskar123 <debasiskar.seo@gmail.com>
2025-03-26 13:24:04 +0530

    Add files via upload

---
+++ assets/js/list.js
@@ -0,0 +1,914 @@
+let list = [
+    {
+      Websites: "https://www.outlookindia.com/",
+      DR: "89",
+      PA: "89",
+      DA: "91",
+      Traffic: "1000"
+    },
+    {
+      Websites: "https://www.jpost.com/",
+      DR: "87",
+      PA: "87",
+      DA: "91",
+      Traffic: "1001"
+    },
+    {
+      Websites: "https://www.digitaljournal.com/",
+      DR: "87",
+      PA: "87",
+      DA: "89",
+      Traffic: "1002"
+    },
+    {
+      Websites: "https://www.khaleejtimes.com/",
+      DR: "83",
+      PA: "83",
+      DA: "86",
+      Traffic: "1003"
+    },
+    {
+      Websites: "https://www.techtimes.com",
+      DR: "83",
+      PA: "83",
+      DA: "82",
+      Traffic: "1004"
+    },
+    {
+      Websites: "https://earthweb.com/",
+      DR: "82",
+      PA: "82",
+      DA: "70",
+      Traffic: "1005"
+    },
+    {
+      Websites: "https://www.tmcnet.com/",
+      DR: "82",
+      PA: "82",
+      DA: "81",
+      Traffic: "1006"
+    },
+    {
+      Websites: "https://www.analyticsinsight.net/",
+      DR: "81",
+      PA: "81",
+      DA: "65",
+      Traffic: "1007"
+    },
+    {
+      Websites: "https://www.colourlovers.com/",
+      DR: "81",
+      PA: "81",
+      DA: "83",
+      Traffic: "1008"
+    },
+    {
+      Websites: "https://fundly.com/",
+      DR: "80",
+      PA: "80",
+      DA: "72",
+      Traffic: "1009"
+    },
+    {
+      Websites: "https://www.cpomagazine.com/",
+      DR: "80",
+      PA: "80",
+      DA: "54",
+      Traffic: "1010"
+    },
+    {
+      Websites: "https://www.ipsnews.net/",
+      DR: "80",
+      PA: "80",
+      DA: "82",
+      Traffic: "1011"
+    },
+    {
+      Websites: "https://www.globalbankingandfinance.com/",
+      DR: "79",
+      PA: "79",
+      DA: "79",
+      Traffic: "1012"
+    },
+    {
+      Websites: "https://goodmenproject.com/",
+      DR: "79",
+      PA: "79",
+      DA: "72",
+      Traffic: "1013"
+    },
+    {
+      Websites: "https://www.widgetbox.com/",
+      DR: "79",
+      PA: "79",
+      DA: "74",
+      Traffic: "1014"
+    },
+    {
+      Websites: "https://menafn.com/",
+      DR: "78",
+      PA: "78",
+      DA: "79",
+      Traffic: "1015"
+    },
+    {
+      Websites: "https://azbigmedia.com/",
+      DR: "78",
+      PA: "78",
+      DA: "65",
+      Traffic: "1016"
+    },
+    {
+      Websites: "http://artdaily.com/",
+      DR: "78",
+      PA: "78",
+      DA: "63",
+      Traffic: "1017"
+    },
+    {
+      Websites: "https://ceoworld.biz/",
+      DR: "77",
+      PA: "77",
+      DA: "72",
+      Traffic: "1018"
+    },
+    {
+      Websites: "https://www.siliconindia.com/",
+      DR: "77",
+      PA: "77",
+      DA: "63",
+      Traffic: "1019"
+    },
+    {
+      Websites: "https://www.skillsyouneed.com/",
+      DR: "77",
+      PA: "77",
+      DA: "60",
+      Traffic: "1020"
+    },
+    {
+      Websites: "https://blinklist.com/",
+      DR: "77",
+      PA: "77",
+      DA: "61",
+      Traffic: "1021"
+    },
+    {
+      Websites: "http://www.finsmes.com/",
+      DR: "77",
+      PA: "77",
+      DA: "59",
+      Traffic: "1022"
+    },
+    {
+      Websites: "https://www.devdiscourse.com/",
+      DR: "77",
+      PA: "77",
+      DA: "63",
+      Traffic: "1023"
+    },
+    {
+      Websites: "https://www.israelnationalnews.com/",
+      DR: "77",
+      PA: "77",
+      DA: "77",
+      Traffic: "1024"
+    },
+    {
+      Websites: "https://realtytimes.com/",
+      DR: "77",
+      PA: "77",
+      DA: "58",
+      Traffic: "1025"
+    },
+    {
+      Websites: "https://cyprus-mail.com/",
+      DR: "76",
+      PA: "76",
+      DA: "72",
+      Traffic: "1026"
+    },
+    {
+      Websites: "https://dailyiowan.com/",
+      DR: "76",
+      PA: "76",
+      DA: "71",
+      Traffic: "1027"
+    },
+    {
+      Websites: "https://coinspeaker.com/",
+      DR: "76",
+      PA: "76",
+      DA: "74",
+      Traffic: "1028"
+    },
+    {
+      Websites: "https://dailycal.org/",
+      DR: "76",
+      PA: "76",
+      DA: "70",
+      Traffic: "1029"
+    },
+    {
+      Websites: "https://www.apnnews.com/",
+      DR: "76",
+      PA: "76",
+      DA: "55",
+      Traffic: "1030"
+    },
+    {
+      Websites: "https://thenationonlineng.net/",
+      DR: "76",
+      PA: "76",
+      DA: "83",
+      Traffic: "1031"
+    },
+    {
+      Websites: "https://beforeitsnews.com/",
+      DR: "76",
+      PA: "76",
+      DA: "76",
+      Traffic: "1032"
+    },
+    {
+      Websites: "https://www.bignewsnetwork.com/",
+      DR: "76",
+      PA: "76",
+      DA: "66",
+      Traffic: "1033"
+    },
+    {
+      Websites: "https://goodereader.com/blog/",
+      DR: "75",
+      PA: "75",
+      DA: "82",
+      Traffic: "1034"
+    },
+    {
+      Websites: "https://www.thesouthafrican.com/",
+      DR: "75",
+      PA: "75",
+      DA: "80",
+      Traffic: "1035"
+    },
+    {
+      Websites: "https://invezz.com/",
+      DR: "75",
+      PA: "75",
+      DA: "76",
+      Traffic: "1036"
+    },
+    {
+      Websites: "https://www.traveldailynews.com/",
+      DR: "75",
+      PA: "75",
+      DA: "61",
+      Traffic: "1037"
+    },
+    {
+      Websites: "https://signalscv.com/",
+      DR: "75",
+      PA: "75",
+      DA: "75",
+      Traffic: "1038"
+    },
+    {
+      Websites: "http://www.hometownstation.com/",
+      DR: "75",
+      PA: "75",
+      DA: "63",
+      Traffic: "1039"
+    },
+    {
+      Websites: "https://www.tgdaily.com",
+      DR: "75",
+      PA: "75",
+      DA: "71",
+      Traffic: "1040"
+    },
+    {
+      Websites: "https://moneyinc.com/",
+      DR: "74",
+      PA: "74",
+      DA: "68",
+      Traffic: "1041"
+    },
+    {
+      Websites: "https://www.fingerlakes1.com/",
+      DR: "74",
+      PA: "74",
+      DA: "52",
+      Traffic: "1042"
+    },
+    {
+      Websites: "https://filmthreat.com/",
+      DR: "74",
+      PA: "74",
+      DA: "61",
+      Traffic: "1043"
+    },
+    {
+      Websites: "https://www.igeeksblog.com",
+      DR: "74",
+      PA: "74",
+      DA: "67",
+      Traffic: "1044"
+    },
+    {
+      Websites: "https://www.webdesign.org/",
+      DR: "74",
+      PA: "74",
+      DA: "54",
+      Traffic: "1045"
+    },
+    {
+      Websites: "https://thesmallbusinessblog.net/",
+      DR: "74",
+      PA: "74",
+      DA: "51",
+      Traffic: "1046"
+    },
+    {
+      Websites: "http://nativenewsonline.net/",
+      DR: "74",
+      PA: "74",
+      DA: "64",
+      Traffic: "1047"
+    },
+    {
+      Websites: "https://www.couponupto.com/",
+      DR: "74",
+      PA: "74",
+      DA: "65",
+      Traffic: "1048"
+    },
+    {
+      Websites: "https://socialmediaexplorer.com/",
+      DR: "74",
+      PA: "74",
+      DA: "65",
+      Traffic: "1049"
+    },
+    {
+      Websites: "https://tweakyourbiz.com/",
+      DR: "74",
+      PA: "74",
+      DA: "56",
+      Traffic: "1050"
+    },
+    {
+      Websites: "https://fintechzoom.com/",
+      DR: "74",
+      PA: "74",
+      DA: "68",
+      Traffic: "1051"
+    },
+    {
+      Websites: "https://www.seventech.org/",
+      DR: "74",
+      PA: "74",
+      DA: "59",
+      Traffic: "1052"
+    },
+    {
+      Websites: "https://www.techolac.com/",
+      DR: "74",
+      PA: "74",
+      DA: "63",
+      Traffic: "1053"
+    },
+    {
+      Websites: "https://urbanmatter.com/",
+      DR: "74",
+      PA: "74",
+      DA: "55",
+      Traffic: "1054"
+    },
+    {
+      Websites: "https://www.ilounge.com/",
+      DR: "74",
+      PA: "74",
+      DA: "82",
+      Traffic: "1055"
+    },
+    {
+      Websites: "https://filmdaily.co/",
+      DR: "73",
+      PA: "73",
+      DA: "63",
+      Traffic: "1056"
+    },
+    {
+      Websites: "https://www.androidguys.com/",
+      DR: "73",
+      PA: "73",
+      DA: "80",
+      Traffic: "1057"
+    },
+    {
+      Websites: "https://www.contactmusic.com/",
+      DR: "73",
+      PA: "73",
+      DA: "78",
+      Traffic: "1058"
+    },
+    {
+      Websites: "https://addicted2success.com/",
+      DR: "73",
+      PA: "73",
+      DA: "62",
+      Traffic: "1059"
+    },
+    {
+      Websites: "https://www.thelondoneconomic.com/",
+      DR: "73",
+      PA: "73",
+      DA: "71",
+      Traffic: "1060"
+    },
+    {
+      Websites: "https://euroweeklynews.com/",
+      DR: "73",
+      PA: "73",
+      DA: "69",
+      Traffic: "1061"
+    },
+    {
+      Websites: "https://www.cheese.com/",
+      DR: "73",
+      PA: "73",
+      DA: "66",
+      Traffic: "1062"
+    },
+    {
+      Websites: "https://inkbotdesign.com/",
+      DR: "73",
+      PA: "73",
+      DA: "58",
+      Traffic: "1063"
+    },
+    {
+      Websites: "https://www.techdee.com/",
+      DR: "73",
+      PA: "73",
+      DA: "74",
+      Traffic: "1064"
+    },
+    {
+      Websites: "https://spacecoastdaily.com/",
+      DR: "73",
+      PA: "73",
+      DA: "75",
+      Traffic: "1065"
+    },
+    {
+      Websites: "https://taxguru.in/",
+      DR: "73",
+      PA: "73",
+      DA: "52",
+      Traffic: "1066"
+    },
+    {
+      Websites: "https://www.redmondpie.com/",
+      DR: "73",
+      PA: "73",
+      DA: "78",
+      Traffic: "1067"
+    },
+    {
+      Websites: "https://www.sitepronews.com/",
+      DR: "73",
+      PA: "73",
+      DA: "58",
+      Traffic: "1068"
+    },
+    {
+      Websites: "https://www.econotimes.com/",
+      DR: "73",
+      PA: "73",
+      DA: "69",
+      Traffic: "1069"
+    },
+    {
+      Websites: "https://londonlovesbusiness.com/",
+      DR: "73",
+      PA: "73",
+      DA: "59",
+      Traffic: "1070"
+    },
+    {
+      Websites: "https://www.e-architect.com/",
+      DR: "73",
+      PA: "73",
+      DA: "80",
+      Traffic: "1071"
+    },
+    {
+      Websites: "https://inspirationfeed.com/",
+      DR: "73",
+      PA: "73",
+      DA: "72",
+      Traffic: "1072"
+    },
+    {
+      Websites: "https://www.baltictimes.com/",
+      DR: "72",
+      PA: "72",
+      DA: "63",
+      Traffic: "1073"
+    },
+    {
+      Websites: "https://socialnomics.net/",
+      DR: "72",
+      PA: "72",
+      DA: "56",
+      Traffic: "1074"
+    },
+    {
+      Websites: "https://knowtechie.com/",
+      DR: "72",
+      PA: "72",
+      DA: "53",
+      Traffic: "1075"
+    },
+    {
+      Websites: "https://www.mobileappdaily.com",
+      DR: "72",
+      PA: "72",
+      DA: "67",
+      Traffic: "1076"
+    },
+    {
+      Websites: "https://fashionweekonline.com/",
+      DR: "72",
+      PA: "72",
+      DA: "64",
+      Traffic: "1077"
+    },
+    {
+      Websites: "https://www.inyourpocket.com/",
+      DR: "72",
+      PA: "72",
+      DA: "72",
+      Traffic: "1078"
+    },
+    {
+      Websites: "https://www.bitrebels.com/",
+      DR: "72",
+      PA: "72",
+      DA: "65",
+      Traffic: "1079"
+    },
+    {
+      Websites: "https://computingforgeeks.com",
+      DR: "72",
+      PA: "72",
+      DA: "48",
+      Traffic: "1080"
+    },
+    {
+      Websites: "https://www.music-news.com/",
+      DR: "72",
+      PA: "72",
+      DA: "67",
+      Traffic: "1081"
+    },
+    {
+      Websites: "https://homebusinessmag.com/",
+      DR: "72",
+      PA: "72",
+      DA: "56",
+      Traffic: "1082"
+    },
+    {
+      Websites: "https://technoxyz.com/",
+      DR: "72",
+      PA: "72",
+      DA: "56",
+      Traffic: "1083"
+    },
+    {
+      Websites: "https://www.davidicke.com/",
+      DR: "71",
+      PA: "71",
+      DA: "76",
+      Traffic: "1084"
+    },
+    {
+      Websites: "https://www.techpocket.org/",
+      DR: "71",
+      PA: "71",
+      DA: "64",
+      Traffic: "1085"
+    },
+    {
+      Websites: "https://www.smartdatacollective.com/",
+      DR: "71",
+      PA: "71",
+      DA: "60",
+      Traffic: "1086"
+    },
+    {
+      Websites: "https://studybreaks.com/",
+      DR: "72",
+      PA: "72",
+      DA: "66",
+      Traffic: "1087"
+    },
+    {
+      Websites: "http://www.architectureartdesigns.com/",
+      DR: "71",
+      PA: "71",
+      DA: "67",
+      Traffic: "1088"
+    },
+    {
+      Websites: "https://www.bbntimes.com/",
+      DR: "71",
+      PA: "71",
+      DA: "72",
+      Traffic: "1089"
+    },
+    {
+      Websites: "https://www.ronaldo7.net/",
+      DR: "71",
+      PA: "71",
+      DA: "81",
+      Traffic: "1090"
+    },
+    {
+      Websites: "http://www.business-opportunities.biz/",
+      DR: "71",
+      PA: "71",
+      DA: "69",
+      Traffic: "1091"
+    },
+    {
+      Websites: "https://www.hubtech.org/",
+      DR: "71",
+      PA: "71",
+      DA: "56",
+      Traffic: "1092"
+    },
+    {
+      Websites: "https://www.bkreader.com/",
+      DR: "71",
+      PA: "71",
+      DA: "56",
+      Traffic: "1093"
+    },
+    {
+      Websites: "https://www.boho-weddings.com/",
+      DR: "71",
+      PA: "71",
+      DA: "56",
+      Traffic: "1094"
+    },
+    {
+      Websites: "https://startupxplore.com/en",
+      DR: "71",
+      PA: "71",
+      DA: "58",
+      Traffic: "1095"
+    },
+    {
+      Websites: "https://www.nogentech.org/",
+      DR: "71",
+      PA: "71",
+      DA: "68",
+      Traffic: "1096"
+    },
+    {
+      Websites: "https://newswire.net/",
+      DR: "71",
+      PA: "71",
+      DA: "58",
+      Traffic: "1097"
+    },
+    {
+      Websites: "https://getintopc.com/",
+      DR: "70",
+      PA: "70",
+      DA: "77",
+      Traffic: "1098"
+    },
+    {
+      Websites: "https://www.theautochannel.com/",
+      DR: "70",
+      PA: "70",
+      DA: "67",
+      Traffic: "1099"
+    },
+    {
+      Websites: "https://washingtonindependent.org/",
+      DR: "70",
+      PA: "70",
+      DA: "61",
+      Traffic: "1100"
+    },
+    {
+      Websites: "https://jt.org/",
+      DR: "70",
+      PA: "70",
+      DA: "63",
+      Traffic: "1101"
+    },
+    {
+      Websites: "https://www.thebrokebackpacker.com/",
+      DR: "70",
+      PA: "70",
+      DA: "60",
+      Traffic: "1102"
+    },
+    {
+      Websites: "https://www.savingadvice.com/",
+      DR: "70",
+      PA: "70",
+      DA: "64",
+      Traffic: "1103"
+    },
+    {
+      Websites: "https://www.globalbrandsmagazine.com/",
+      DR: "70",
+      PA: "70",
+      DA: "66",
+      Traffic: "1104"
+    },
+    {
+      Websites: "https://twitgoo.com/",
+      DR: "70",
+      PA: "70",
+      DA: "63",
+      Traffic: "1105"
+    },
+    {
+      Websites: "https://talkmarkets.com/",
+      DR: "69",
+      PA: "69",
+      DA: "52",
+      Traffic: "1106"
+    },
+    {
+      Websites: "https://www.noobpreneur.com/",
+      DR: "69",
+      PA: "69",
+      DA: "54",
+      Traffic: "1107"
+    },
+    {
+      Websites: "https://cannabis.net/",
+      DR: "69",
+      PA: "69",
+      DA: "55",
+      Traffic: "1108"
+    },
+    {
+      Websites: "https://www.companionlink.com/blog/",
+      DR: "69",
+      PA: "69",
+      DA: "76",
+      Traffic: "1109"
+    },
+    {
+      Websites: "https://fashionbombdaily.com/",
+      DR: "68",
+      PA: "68",
+      DA: "66",
+      Traffic: "1110"
+    },
+    {
+      Websites: "https://www.newsanyway.com/",
+      DR: "68",
+      PA: "68",
+      DA: "62",
+      Traffic: "1111"
+    },
+    {
+      Websites: "https://www.trueactivist.com/",
+      DR: "68",
+      PA: "68",
+      DA: "69",
+      Traffic: "1112"
+    },
+    {
+      Websites: "https://d-addicts.com/",
+      DR: "68",
+      PA: "68",
+      DA: "72",
+      Traffic: "1113"
+    },
+    {
+      Websites: "https://www.freelancewritinggigs.com/",
+      DR: "68",
+      PA: "68",
+      DA: "57",
+      Traffic: "1114"
+    },
+    {
+      Websites: "https://ragezone.com/",
+      DR: "68",
+      PA: "68",
+      DA: "52",
+      Traffic: "1115"
+    },
+    {
+      Websites: "https://www.designscene.net/",
+      DR: "67",
+      PA: "67",
+      DA: "72",
+      Traffic: "1116"
+    },
+    {
+      Websites: "https://lerablog.org/",
+      DR: "67",
+      PA: "67",
+      DA: "71",
+      Traffic: "1117"
+    },
+    {
+      Websites: "https://moneymagpie.com/",
+      DR: "67",
+      PA: "67",
+      DA: "54",
+      Traffic: "1118"
+    },
+    {
+      Websites: "https://analyticssteps.com/",
+      DR: "66",
+      PA: "66",
+      DA: "43",
+      Traffic: "1119"
+    },
+    {
+      Websites: "https://lastwordonsports.com/",
+      DR: "66",
+      PA: "66",
+      DA: "64",
+      Traffic: "1120"
+    },
+    {
+      Websites: "https://www.getfootballnewsfrance.com/",
+      DR: "65",
+      PA: "65",
+      DA: "68",
+      Traffic: "1121"
+    },
+    {
+      Websites: "https://www.greenprophet.com/",
+      DR: "65",
+      PA: "65",
+      DA: "66",
+      Traffic: "1122"
+    },
+    {
+      Websites: "https://www.thefashionablehousewife.com/",
+      DR: "64",
+      PA: "64",
+      DA: "56",
+      Traffic: "1123"
+    }
+  ]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let list_table = document.getElementById("domain-list");
+
+list.forEach(element => {
+    list_table.innerHTML += `
+<tr>
+<td colspan="2">${element.Websites}</td>
+<td>${element.DA}</td>
+<td>${element.PA}</td>
+<td>${element.DR}</td>
+<td>${element.Traffic}</td>
+<td>${element.PA}</td>
+<td>${element.DR}</td>
+</tr>
+    `
+});
